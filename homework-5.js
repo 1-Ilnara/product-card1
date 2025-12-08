24bd0fe7318dc629911debeb83dadd6ed20086a3
1-ilnara <hajrulinailnara@gmail.comhajrulinailnara@gmail.com>
2025-12-08 15:18:51 +0000

    fixed errors

--- homework-5.js
+++ homework-5.js
@@ -1,4 +1,56 @@
-function logTemperature(city, temperature) {
+const productCard = document.querySelectorAll('.card-container');
+const changeColorAllCardButton = document.querySelector('#change-color-all-card-button');
+const greenColorHash = '#00ff00'
+const blueColorHash = '#0000FF'
+ 
+
+ 
+changeColorAllCardButton.addEventListener('click',() => {
+productCard.forEach((card) => card.style.backgroundColor = greenColorHash)
+})
+
+console.log('ProductCard', productCard)
+console.log('Button', changeColorAllCardButton)
+
+const firstProductCard = document.querySelector('.card-container');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card-button');
+
+changeColorFirstCardButton.addEventListener('click',() => {
+firstProductCard.style.backgroundColor = blueColorHash;
+})
+
+const openGoogleButton = document.querySelector('#open-google');
+openGoogleButton.addEventListener('click',openGoogle)
+function openGoogle() {
+    const answer = confirm('Вы действительно хотите открыть Google?');
+
+    if (answer === true) {
+        window.open('https://google.com')
+    } else {
+        return;
+    }
+}
+
+const toggleButton = document.getElementById('color-toggle-button');
+
+toggleButton.classList.add('btn-style', 'text-white', 'bg-blue');
+
+toggleButton.addEventListener('click', function() {
+    this.classList.toggle('bg-blue');
+    this.classList.toggle('bg-red');
+});
+
+const mainTitleButton = document.getElementById('main-title-button');
+const mainTitleElement = document.querySelector('.main-title');
+
+if (mainTitleButton && mainTitleElement) {
+    mainTitleButton.addEventListener('click', () => {
+        console.log(mainTitleElement.textContent);
+    });
+
+
+
+}function logTemperature(city, temperature) {
   console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 }
 
@@ -45,8 +97,11 @@ attemptToPurchase(75000);
 const userFirstName = "Ilnara";
 const userLastName = "Khairulina";
 let userAge = 35;
-function generateWelcomeMessage (){
-  const message =`Добро пожаловать, ${userFirstName}${userLastName}${userAge}`;
+
+function generateWelcomeMessage() { 
+  const message =`Добро пожаловать, ${userFirstName} ${userLastName}, возраст: ${userAge}`;   
   console.log(`\n---Приветственное сообщение---`);
-  console (message);
+  console.log(message); 
 }
+
+generateWelcomeMessage();
