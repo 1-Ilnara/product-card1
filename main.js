d41d2c11d9ea51da21472be2c4897c802cc1336a
1-ilnara <hajrulinailnara@gmail.comhajrulinailnara@gmail.com>
2026-02-28 16:36:25 +0000

    fix

--- main.js
+++ main.js
@@ -30,3 +30,4 @@ if (openBtn) {
     openBtn.addEventListener('click', handleOpen);
 }
 
+
--- modal.js
+++ modal.js
@@ -3,26 +3,22 @@ export class Modal {
     this.modal = document.getElementById(id);
     this.closeBtn = this.modal.querySelector(".close-btn");
     this.handleCloseClick = this.close.bind(this);
-    this.init();
   }
   open() {
     this.modal.style.display = "block";
+    this.init();    
   }
   close() {
     this.modal.style.display = "none";
-    this.destroy(); 
+    this.destroy();  
   }
   isOpen() {
     return this.modal.style.display === "block";
   }
   init() {
-    if (this.closeBtn) {
-      this.closeBtn.addEventListener("click", this.handleCloseClick);
-    }
+    this.closeBtn?.addEventListener("click", this.handleCloseClick);
   }
   destroy() {
-    if (this.closeBtn) {
-      this.closeBtn.removeEventListener("click", this.handleCloseClick);
-    }
+    this.closeBtn?.removeEventListener("click", this.handleCloseClick);
   }
 }
